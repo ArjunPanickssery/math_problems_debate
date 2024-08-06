0dba25526f22129e887683357563249e0a1bffc7
abhimanyu <abhimanyupallavisudhir@gmail.com>
2024-08-06 23:44:23 +0100

    fixes

--- src/protocols/variants/consultancy.py
+++ src/protocols/variants/consultancy.py
@@ -1,4 +1,4 @@
-from datatypes import Answer, Question
+from src.datatypes import Answer, Question
 from src.protocols.consultancy import Consultant, Client, Consultancy
 from src.protocols.common import Judge, Transcript
 
@@ -7,6 +7,6 @@ class OpenConsultancy(Consultancy):
     def choose_answer(self, question: Question) -> Answer:
         auxillary_judge = Judge(model=self.consultant.model)
         transcript = Transcript(question)
-        probabilities = auxillary_judge(transcript).judgement.probabilities
+        probabilities = auxillary_judge(transcript).probabilities
         ans_key = max(probabilities, key=probabilities.get)
         return question.possible_answers_dict[ans_key]
