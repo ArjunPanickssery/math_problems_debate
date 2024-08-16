37a4245966785852a34bbb4c8ce22844f8e99352
abhimanyu <abhimanyupallavisudhir@gmail.com>
2024-08-16 01:04:26 +0100

    more tests

--- tests/test_cost_estimator.py
+++ tests/test_cost_estimator.py
@@ -2,24 +2,60 @@ import pytest
 from solib.cost_estimator import CostItem, CostEstimator
 from solib.llm_utils import get_llm_response, get_llm_response_async
 
-PROMPT = (
+PROMPT1 = (
     "Explain the Riemann Hypothesis to me in full mathematical "
     "detail, including all necessary prerequisites."
 )
+PROMPT2 = (
+    "Take a random guess as to what the 1,000,001st digit of pi is. "
+    'Answer exactly "0", "1", ... or "9", with nothing else in your response.'
+)
+
+PARAMS = [
+    {"prompt": PROMPT1, "model": "gpt-4o-mini", "return_probs_for": None},
+    {
+        "prompt": PROMPT2,
+        "model": "gpt-4o-mini",
+        "return_probs_for": [str(n) for n in range(10)],
+    },
+    {
+        "prompt": PROMPT1,
+        "model": "hf:meta-llama/Llama-2-7b-chat-hf",
+        "return_probs_for": None,
+    },
+    {
+        "prompt": PROMPT2,
+        "model": "hf:meta-llama/Llama-2-7b-chat-hf",
+        "return_probs_for": [str(n) for n in range(10)],
+    },
+]
 
 
-def test_estimate_contains_exact():
+@pytest.mark.parametrize("params", PARAMS)
+def test_estimate_contains_exact(params, request):
+
+    prompt, model, return_probs_for = (
+        params["prompt"],
+        params["model"],
+        params["return_probs_for"],
+    )
+
+    if model.startswith("hf:") and not request.config.getoption("--runhf", False):
+        pytest.skip("skipping test with hf model, add --runhf option to run")
+
     ce_sim = CostEstimator()
     ce_real = CostEstimator()
     x = get_llm_response(
-        PROMPT,
-        model="gpt-4o-mini",
+        prompt,
+        model=model,
+        return_probs_for=return_probs_for,
         simulate=True,
         cost_estimation={"cost_estimator": ce_sim},
     )
     y = get_llm_response(
-        PROMPT,
-        model="gpt-4o-mini",
+        prompt,
+        model=model,
+        return_probs_for=return_probs_for,
         simulate=False,
         cost_estimation={"cost_estimator": ce_real},
     )
