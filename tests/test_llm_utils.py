572baadc84a8a024348917a72fe24e3dbcad15c5
abhimanyupallavisudhir <abhimanyupallavisudhir@gmail.com>
2024-11-09 16:28:12 +0000

    fix broken tests

--- tests/test_llm_utils.py
+++ tests/test_llm_utils.py
@@ -17,22 +17,27 @@ def model(request):
 
 
 @pytest.fixture
-def llm_agent(model):
-    return LLM_Agent(model=model)
+def llm_agent_sync(model):
+    return LLM_Agent(model=model, sync_mode=True)
 
 
-def test_get_response_returns_string(llm_agent, request):
-    if llm_agent.model.startswith("hf:") and not request.config.getoption(
+@pytest.fixture
+def llm_agent_async(model):
+    return LLM_Agent(model=model, sync_mode=False)
+
+
+def test_get_response_returns_string(llm_agent_sync, request):
+    if llm_agent_sync.model.startswith("hf:") and not request.config.getoption(
         "--runhf", False
     ):
         pytest.skip("slow test, add --runhf option to run")
     prompt = "What is the capital of France?"
-    response = llm_agent.get_response_sync(prompt=prompt)
+    response = llm_agent_sync.get_response_sync(prompt=prompt)
     assert isinstance(response, str)
 
 
-def test_get_probs_returns_dict(llm_agent, request):
-    if llm_agent.model.startswith("hf:") and not request.config.getoption(
+def test_get_probs_returns_dict(llm_agent_sync, request):
+    if llm_agent_sync.model.startswith("hf:") and not request.config.getoption(
         "--runhf", False
     ):
         pytest.skip("skipping test with hf model, add --runhf option to run")
@@ -40,27 +45,27 @@ def test_get_probs_returns_dict(llm_agent, request):
         "Take a random guess as to what the 1,000,001st digit of pi is. "
         'Answer exactly "0", "1", ... or "9", with nothing else in your response.'
     )
-    response = llm_agent.get_probs_sync(
+    response = llm_agent_sync.get_probs_sync(
         prompt=prompt,
         return_probs_for=[str(n) for n in range(10)],
     )
     assert isinstance(response, dict)
 
 
-def test_get_response_with_max_tokens(llm_agent, request):
-    if llm_agent.model.startswith("hf:") and not request.config.getoption(
+def test_get_response_with_max_tokens(llm_agent_sync, request):
+    if llm_agent_sync.model.startswith("hf:") and not request.config.getoption(
         "--runhf", False
     ):
         pytest.skip("slow test, add --runhf option to run")
     prompt = "What is the capital of France?"
     max_tokens = 50
-    response = llm_agent.get_response_sync(prompt=prompt, max_tokens=max_tokens)
+    response = llm_agent_sync.get_response_sync(prompt=prompt, max_tokens=max_tokens)
     assert isinstance(response, str)
     assert len(response.split()) <= max_tokens
 
 
-def test_get_response_with_words_in_mouth(llm_agent, request):
-    if llm_agent.model.startswith("hf:") and not request.config.getoption(
+def test_get_response_with_words_in_mouth(llm_agent_sync, request):
+    if llm_agent_sync.model.startswith("hf:") and not request.config.getoption(
         "--runhf", False
     ):
         pytest.skip("slow test, add --runhf option to run")
@@ -69,62 +74,66 @@ def test_get_response_with_words_in_mouth(llm_agent, request):
         'Answer exactly "0", "1", ... or "9", with nothing else in your response.'
     )
     words_in_mouth = " My guess for the correct answer is:\n\n"
-    response = llm_agent.get_response_sync(prompt=prompt, words_in_mouth=words_in_mouth)
-    assert isinstance(response, str)
-
-
-@pytest.mark.asyncio
-async def test_get_response_async_returns_string(llm_agent, request):
-    if llm_agent.model.startswith("hf:"):
-        pytest.skip("hf models don't support async")
-    prompt = "What is the capital of France?"
-    response = await llm_agent.get_response_async(prompt=prompt)
-    assert isinstance(response, str)
-
-
-@pytest.mark.asyncio
-async def test_get_probs_async_returns_dict(llm_agent, request):
-    if llm_agent.model.startswith("hf:"):
-        pytest.skip("hf models don't support async")
-    prompt = (
-        "Take a random guess as to what the 1,000,001st digit of pi is. "
-        'Answer exactly "0", "1", ... or "9", with nothing else in your response.'
-    )
-    response = await llm_agent.get_probs_async(
-        prompt=prompt,
-        return_probs_for=[str(n) for n in range(10)],
-    )
-    assert isinstance(response, dict)
-
-
-@pytest.mark.asyncio
-async def test_get_response_async_with_max_tokens(llm_agent, request):
-    if llm_agent.model.startswith("hf:"):
-        pytest.skip("hf models don't support async")
-    prompt = "What is the capital of France?"
-    max_tokens = 50
-    response = await llm_agent.get_response_async(prompt=prompt, max_tokens=max_tokens)
-    assert isinstance(response, str)
-    assert len(response.split()) <= max_tokens
-
-
-@pytest.mark.asyncio
-async def test_get_response_async_with_words_in_mouth(llm_agent, request):
-    if llm_agent.model.startswith("hf:"):
-        pytest.skip("hf models don't support async")
-    prompt = (
-        "Take a random guess as to what the 1,000,001st digit of pi is. "
-        'Answer exactly "0", "1", ... or "9", with nothing else in your response.'
-    )
-    words_in_mouth = " My guess for the correct answer is:\n\n"
-    response = await llm_agent.get_response_async(
+    response = llm_agent_sync.get_response_sync(
         prompt=prompt, words_in_mouth=words_in_mouth
     )
     assert isinstance(response, str)
 
 
-def test_get_response_with_response_model(llm_agent, request):
-    if llm_agent.model.startswith("hf:") and not request.config.getoption(
+@pytest.mark.asyncio
+async def test_get_response_async_returns_string(llm_agent_async, request):
+    if llm_agent_async.model.startswith("hf:"):
+        pytest.skip("hf models don't support async")
+    prompt = "What is the capital of France?"
+    response = await llm_agent_async.get_response_async(prompt=prompt)
+    assert isinstance(response, str)
+
+
+@pytest.mark.asyncio
+async def test_get_probs_async_returns_dict(llm_agent_async, request):
+    if llm_agent_async.model.startswith("hf:"):
+        pytest.skip("hf models don't support async")
+    prompt = (
+        "Take a random guess as to what the 1,000,001st digit of pi is. "
+        'Answer exactly "0", "1", ... or "9", with nothing else in your response.'
+    )
+    response = await llm_agent_async.get_probs_async(
+        prompt=prompt,
+        return_probs_for=[str(n) for n in range(10)],
+    )
+    assert isinstance(response, dict)
+
+
+@pytest.mark.asyncio
+async def test_get_response_async_with_max_tokens(llm_agent_async, request):
+    if llm_agent_async.model.startswith("hf:"):
+        pytest.skip("hf models don't support async")
+    prompt = "What is the capital of France?"
+    max_tokens = 50
+    response = await llm_agent_async.get_response_async(
+        prompt=prompt, max_tokens=max_tokens
+    )
+    assert isinstance(response, str)
+    assert len(response.split()) <= max_tokens
+
+
+@pytest.mark.asyncio
+async def test_get_response_async_with_words_in_mouth(llm_agent_async, request):
+    if llm_agent_async.model.startswith("hf:"):
+        pytest.skip("hf models don't support async")
+    prompt = (
+        "Take a random guess as to what the 1,000,001st digit of pi is. "
+        'Answer exactly "0", "1", ... or "9", with nothing else in your response.'
+    )
+    words_in_mouth = " My guess for the correct answer is:\n\n"
+    response = await llm_agent_async.get_response_async(
+        prompt=prompt, words_in_mouth=words_in_mouth
+    )
+    assert isinstance(response, str)
+
+
+def test_get_response_with_response_model(llm_agent_sync, request):
+    if llm_agent_sync.model.startswith("hf:") and not request.config.getoption(
         "--runhf", False
     ):
         pytest.skip("slow test, add --runhf option to run")
@@ -134,7 +143,9 @@ def test_get_response_with_response_model(llm_agent, request):
         country: str
 
     prompt = "What is the capital of France?"
-    response = llm_agent.get_response_sync(prompt=prompt, response_model=ResponseModel)
+    response = llm_agent_sync.get_response_sync(
+        prompt=prompt, response_model=ResponseModel
+    )
 
     # need to check instance weirdly since cloudpickle hack messes up class namespace a bit
     assert type(response).__name__ == ResponseModel.__name__
@@ -143,8 +154,8 @@ def test_get_response_with_response_model(llm_agent, request):
 
 
 @pytest.mark.asyncio
-async def test_get_response_async_with_response_model(llm_agent, request):
-    if llm_agent.model.startswith("hf:"):
+async def test_get_response_async_with_response_model(llm_agent_async, request):
+    if llm_agent_async.model.startswith("hf:"):
         pytest.skip("hf models don't support async")
 
     class ResponseModel(BaseModel):
@@ -153,7 +164,7 @@ async def test_get_response_async_with_response_model(llm_agent, request):
 
     prompt = "What is the capital of France?"
 
-    response = await llm_agent.get_response_async(
+    response = await llm_agent_async.get_response_async(
         prompt=prompt, response_model=ResponseModel
     )
 
